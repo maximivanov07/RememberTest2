5348ba72f7671c41f8a7ef3abeeb4c7f57a6b618
Maksym Ivanov <maxim.ivanov.89@hotmail.com>
2018-08-10 17:59:32 +0300

    Change name of project, separate projects, modify

--- RememberTest2/Program.cs
+++ RememberTest2/Program.cs
@@ -3,99 +3,222 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Collections;
 
-namespace RememberTest2
+namespace UtitCalc
 {
+    
     class Program
     {
+
         static void Main(string[] args)
         {
-            //Nontype();
-            //MasObjIdent();
-            //MasMultyArray();
-            //PassAndReciveArrays();
-            AriphProgr();
+            try
+            {
+                Menu();
+            }
+            catch (Exception e)
+            {
+                Console.Clear();
+                Console.WriteLine("Ты ввел хуйню дружище, наверное опять жмакнул Enter {0}", e);
+            }
 
             Console.ReadLine();
-
         }
-        static void Nontype()
+
+        //Составьте программы перевода старинных русских мер длины, торгового и аптекарского веса(счетчик цикла от 1 до 10)
+        //* саженей в метры(1 сажень равна 2,1366 м)
+        //* из дюймов в сантиметры(1 дюйм равен 2,5 см)
+        //* футов в метры(1 фут равен 0,3048 м)
+        //* драхм в граммы(1 драхма равна 3,7325 г)
+        //* унций в граммы(1 унция равна 29,86 г)
+        //* фунтов в килограммы(1 фунт равен 0,40951 кг)
+        //* аршинов в метры(1 аршин равен 0,7112 м)
+        //* золотников в граммы(1 золотник равен 4,2657 г)
+        //* дюймов в миллиметры(1 дюйм равен 25,3995 мм)
+
+        public static void Menu()
         {
-            var a = new[] { 1, 2, 3, 4 };
+            
+            // Переменная с индексом позиции меню и счетчик
+            short curItem = 0, c;
+            // Объект для чтения кнопки
+            ConsoleKeyInfo key;
+            // Массив пунктов меню
 
-            foreach (int i in a)
-                Console.WriteLine(i);
-            Console.WriteLine(a.Length);
-        }
-        static void MasObjIdent()
-        {
+            //string[] menuItems = { "Сажени", "Метры", "Дюймы", "Сантиметры", "Миллиметры", "Футы", "Аршины" };
+            string[] menuItems = { "Сажени", "Метры", "Дюймы" };
 
-            object[] objects = new object[4];
-            objects[0] = 10;
-            objects[1] = false;
-            objects[2] = new DateTime(1989, 10, 07);
-            objects[3] = "Maksym Ivanov";
-
-            foreach (object obj in objects)
+            do
             {
-                Console.WriteLine("Type: {0}, Value: {1}", obj.GetType(), obj);
-            }
+                Console.Clear();
+                Console.WriteLine("Что конвертируем? \n");
 
+                //Логика отрисовки
+                for (c = 0; c < menuItems.Length; c++)
+                {
+                    //если текущий элемент равен выбранному элементу, то добавляем звездочку
+                    if (curItem == c)
+                    {
+                        Console.Write(" * ");
+                        Console.WriteLine(menuItems[c]);
+                    }
+                    else if (key.Key.ToString() == "Enter")
+                    {
+                        //Console.Clear();
+                        //Console.WriteLine(menuItems[c]);
+                        SwitcherLogic(curItem);
+
+                    }
+                    //Выводим элемент, но без звездочки
+                    else
+                    {
+                        Console.WriteLine(menuItems[c]);
+                    }
+                }
+
+                //логика переключения
+                key = Console.ReadKey(true);
+
+                if (key.Key.ToString() == "DownArrow")
+                {
+                    curItem++;
+                    //если текущий индекс больше чем длинна меню -1, то перевести курсор в начало
+                    if (curItem == menuItems.Length) curItem = 0;
+                }
+                else if (key.Key.ToString() == "UpArrow")
+                {
+                    curItem--;
+                    if (curItem < 0) curItem = Convert.ToInt16(menuItems.Length - 1);
+                }
+                // Выходим из цикла если юзер нажал Enter
+            } 
+            while (key.KeyChar != 13);
+            Console.Clear();
+
+            //how can i make it so if i press enter on one of these it will do a command like
         }
-        static void MasMultyArray()
-        {
-            int[][] myJagArray = new int[5][];
-            for (int i = 0; i < myJagArray.Length; i++)
-                myJagArray[i] = new int[i + 1];
 
-            for (int i = 0; i < 5; i++)
+        //Логика выбора
+        public static void SwitcherLogic(short curItem)
+        {
+            switch (curItem)
             {
-                for (int j = 0; j < myJagArray[i].Length; j++)
-                    Console.Write(myJagArray[i][j] + " ");
-                Console.WriteLine();
+                case 0:
+
+                    Console.Clear();
+                    SagenCalc();
+
+                    break;
+
+                case 1:
+                    
+                    Console.Clear();
+                    MeterCalc();
+
+                    break;
+                
+                case 2:
+
+                    Console.Clear();
+                    InchCalc();
+
+                    break;
+
+                default:
+                    Console.Clear();
+                    Console.WriteLine("Это мы пока считать не умеем");
+                    break;
             }
-            Console.WriteLine();
         }
 
-        ///
-        static void PrintArray(int[] myInts)
+        public static void SagenCalc()
         {
-            for (int i = 0; i < myInts.Length; i++)
-                Console.WriteLine("Item {0} is {1}", i, myInts[i]);
+
+            UnitCalcLogic sagenCalc = new UnitCalcLogic();
+
+            Console.WriteLine("Введи количество саженей и я тебе их посчитаю во всем сразу\n");
+
+            Console.Clear();
+
+            Console.WriteLine("Результат: \n");
+
+            Console.WriteLine("Метрическая система \n");
+            Console.WriteLine("Миллиметров: {0}", sagenCalc.sagenInMilimeter);
+            Console.WriteLine("Сантиметров: {0}", sagenCalc.sagenInSantimeter);
+            Console.WriteLine("Метров: {0}", sagenCalc.sagenInMeter);
+            Console.WriteLine("Километров: {0}\n", sagenCalc.sagenInKilometer);
+
+            Console.WriteLine("Империческая система \n");
+            Console.WriteLine("Дюймов: {0}", sagenCalc.sagenInInch);
+            Console.WriteLine("Футов: {0}", sagenCalc.sagenInFoot);
+            Console.WriteLine("Ярдов: {0}", sagenCalc.sagenInYard);
+            Console.WriteLine("Миль: {0}\n", sagenCalc.sagenInMile);
+
+            Console.WriteLine("Старославянская система \n");
+            Console.WriteLine("Вершков: {0}", sagenCalc.sagenInVershok);
+            Console.WriteLine("Пядей: {0}", sagenCalc.sagenInPyad);
+            Console.WriteLine("Аршинов: {0}", sagenCalc.sagenInArshin);
+            Console.WriteLine("Верст: {0}", sagenCalc.sagenInVersta);
+
         }
-        static string[] GetStringArray()
+
+        public static void MeterCalc()
         {
-            string[] theStrings = { "Hello", "from", "GetStringArray" };
-            return theStrings;
+
+            UnitCalcLogic meterCalc = new UnitCalcLogic();
+
+            Console.WriteLine("Введи количество метров и я тебе их посчитаю во всем сразу\n");
+
+            Console.Clear();
+
+            Console.WriteLine("Результат: \n");
+
+            Console.WriteLine("Метрическая система \n");
+            Console.WriteLine("Миллиметров: {0}", meterCalc.meterInMilimeter);
+            Console.WriteLine("Сантиметров: {0}", meterCalc.meterInSantimeter);
+            Console.WriteLine("Километров: {0}\n", meterCalc.meterInKilometer);
+
+            Console.WriteLine("Империческая система \n");
+            Console.WriteLine("Дюймов: {0}", meterCalc.meterInInch);
+            Console.WriteLine("Футов: {0}", meterCalc.meterInFoot);
+            Console.WriteLine("Ярдов: {0}", meterCalc.meterInYard);
+            Console.WriteLine("Миль: {0}\n", meterCalc.meterInMile);
+
+            Console.WriteLine("Старославянская система \n");
+            Console.WriteLine("Вершков: {0}", meterCalc.meterInVershok);
+            Console.WriteLine("Пядей: {0}", meterCalc.meterInPyad);
+            Console.WriteLine("Аршинов: {0}", meterCalc.meterInArshin);
+            Console.WriteLine("Верст: {0}", meterCalc.meterInVersta);
         }
-        static void PassAndReciveArrays()
+
+        public static void InchCalc()
         {
-            Console.WriteLine("=> Arrays as params and return values.");
-            //peredat massiv
-            int[] ages = { 20, 22, 23, 24, 0 };
-            PrintArray(ages);
-            //give array
-            string[] strs = GetStringArray();
-            foreach (string s in strs)
-                Console.WriteLine(s);
-            Console.WriteLine();
-        }
-        ///
+            
+            UnitCalcLogic inchCalc = new UnitCalcLogic();
 
-        public static void AriphProgr()
-        {
-            int a = 3; //то, что будем возводить в степень
-            int progressionSize = 5; //сколько элементов в формуле (сколько итераций)
-            int result=0; //results
+            Console.WriteLine("Введи количество дюймов и я тебе их посчитаю во всем сразу\n");
 
+            Console.Clear();
 
-            for (int i = 1; i < progressionSize; i++)
-            {
-                int x = Convert.ToInt32(Math.Pow(a, i));
-                result = result + x;
+            Console.WriteLine("Результат: \n");
 
-            }
-            Console.WriteLine(result+1);
+            //Console.WriteLine("Метрическая система \n");
+            //Console.WriteLine("Миллиметров: {0}", meterCalc.meterInMilimeter);
+            //Console.WriteLine("Сантиметров: {0}", meterCalc.meterInSantimeter);
+            //Console.WriteLine("Километров: {0}\n", meterCalc.meterInKilometer);
+
+            Console.WriteLine("Империческая система \n");
+            Console.WriteLine("Футов: {0}", inchCalc.meterInFoot);
+            Console.WriteLine("Ярдов: {0}", inchCalc.meterInYard);
+            Console.WriteLine("Миль: {0}\n", inchCalc.meterInMile);
+
+            //Console.WriteLine("Старославянская система \n");
+            //Console.WriteLine("Вершков: {0}", meterCalc.meterInVershok);
+            //Console.WriteLine("Пядей: {0}", meterCalc.meterInPyad);
+            //Console.WriteLine("Аршинов: {0}", meterCalc.meterInArshin);
+            //Console.WriteLine("Верст: {0}", meterCalc.meterInVersta);
         }
     }
 }
---
+++ RememberTest2/UnitCalcLogic.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Collections;
+
+namespace UtitCalc
+{
+    public class UnitCalcLogic
+    {
+        //sagen
+
+        public float sagenInKilometer;
+        public float sagenInMeter;
+        public float sagenInSantimeter;
+        public float sagenInMilimeter;
+       
+        public float sagenInArshin;
+        public float sagenInPyad;
+        public float sagenInVershok;
+        public float sagenInVersta;
+
+        public float sagenInInch;
+        public float sagenInFoot;
+        public float sagenInYard;
+        public float sagenInMile;
+
+        //meter
+        public float meterInKilometer;
+        public float meterInSantimeter;
+        public float meterInMilimeter;
+
+        public float meterInVersta;
+        public float meterInArshin;
+        public float meterInPyad;
+        public float meterInVershok;
+
+        public float meterInInch;
+        public float meterInFoot;
+        public float meterInYard;
+        public float meterInMile;
+
+        //inch
+        public float inchInFoot;
+        public float inchInYard;
+        public float inchInMile;
+
+        public UnitCalcLogic()
+        {
+        
+        float userInput = float.Parse(Console.ReadLine());
+
+        //sagen
+        sagenInKilometer = 0.0021336F * userInput;
+        sagenInMeter = 2.1366F * userInput;
+        sagenInSantimeter = 213.36F * userInput;
+        sagenInMilimeter = 2133.6F * userInput;
+
+        sagenInArshin = userInput * 3;
+        sagenInPyad = userInput * (userInput / 12);
+        sagenInVershok = userInput * (userInput / 48);
+        sagenInVersta = userInput * 0.002F;
+
+        sagenInInch = 84 * userInput;
+        sagenInFoot = 7 * userInput;
+        sagenInYard = 2.33333F * userInput;
+        sagenInMile = 0.00132576F * userInput;
+        
+        //meter
+        meterInKilometer = userInput / 1000;
+        meterInSantimeter = userInput * 100;
+        meterInMilimeter = userInput * 1000;
+
+        meterInVersta = userInput * 0.000937383F;
+        meterInArshin = userInput * 0.468691F;
+        meterInPyad = userInput * 4.37445F;
+        meterInVershok = userInput * 22.49719F;
+
+        meterInInch = userInput * 39.3701F;
+        meterInFoot = userInput * 3.28084F;
+        meterInYard = userInput * 1.09361F;
+        meterInMile = userInput * 0.000621371F;
+        
+        //inch
+        inchInFoot = userInput * 12;
+        inchInYard = userInput * 36;
+        inchInMile = userInput * 63360;
+
+        }
+    }
+}
+
